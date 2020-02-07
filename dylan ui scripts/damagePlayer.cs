332e855f17905678fdfd99635da4f224c9ceffb8
dylanalvarez1 <dylanalvarez1@ufl.edu>
2020-02-07 13:14:34 -0500

    add ui/functionality for minimap, time, score, hunger
    install with dylan-ui-prototype package on google drive since there's required scene + asset setup

---
+++ dylan ui scripts/damagePlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damagePlayer : MonoBehaviour
+{
+    public GameObject sceneManager;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            sceneManager.SendMessage("damagePlayer");
+        }
+    }
+}
